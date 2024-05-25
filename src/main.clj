f3c9f4a2b0a0c5f6bbff3857ddf293d1a0c6f389
apiyo <franklineapiyo@gmail.com>
2024-05-25 10:28:31 +0300

    Initial commit

---
+++ src/main.clj
@@ -0,0 +1,13 @@
+(ns main
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]
+            [io.pedestal.test :as test]))
+
+(defn response
+  [status body & {:as headers}]
+  {:status status, :body body, :headers headers})
+
+(def ok (partial response 200))
+(def created (partial response 201))
+(def accepted (partial response 202))
+
